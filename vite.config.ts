5e6b9b7c365a49407eadace3de5a8f195dd7bcce
Jose Luis Cabrera <cabrera@inkuba.com>
2022-08-29 11:14:44 -0300

    feat: auto import

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components()],
+  plugins: [
+    vue(), 
+    Components(), 
+    AutoImport({
+      imports: [
+        'vue'
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
