62420a8cc19b03e190af16e5f12d2d198aa59dbc
Jose Luis Cabrera <cabrera@inkuba.com>
2022-08-29 11:43:49 -0300

    feat: setup unocss

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 </script>
 <template>
-<div>
+<div class="text-red text-3xl">
     Hello yess
 </div>
+<div i-carbon-bee text-3xl ></div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import {presetUno, presetAttributify, presetIcons} from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +15,13 @@ export default defineConfig({
     AutoImport({
       imports: [
         'vue'
+      ]
+    }),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
       ]
     })
   ],
